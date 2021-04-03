9cc788497fa2fbc5b3f17a0baa6865537bbaf97a
Felicie Amundaray <feliamunda@outlook.com>
2021-04-03 00:33:41 -0400

    ✨ Generate Interfaces

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    username:string;
+    role:string;
+    name:string;
+    lastName:string;
+    birthday:Date;
+    age:Number;
+    email:string;
+    active:boolean;
+    createdAt:Date;
+    updatedAt:Date;
+}
